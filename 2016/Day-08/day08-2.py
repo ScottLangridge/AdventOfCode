b12076633a6fc7b147d6def573f4af6b96e8edbb
scott <scott@derossi.consulting>
2022-10-20 22:23:40 +0100

    Solved day 8

--- 2016/Day-08/day08-2.py
+++ 2016/Day-08/day08-2.py
@@ -1,10 +1,53 @@
 def main(raw_input):
-    # Parse input
+    instructions = [parse_instruction(i) for i in raw_input.strip().splitlines()]
+    grid = [[' ' for _ in range(50)] for _ in range(6)]
 
-    # Solve problem
+    for i in instructions:
+        grid = i[0](grid, i[1], i[2])
 
-    # Return solution
-    return None
+    return str_grid(grid)
+
+
+def parse_instruction(instruction):
+    split = instruction.split()
+    if len(split) == 2:
+        a, b = [int(i) for i in split[1].split('x')]
+        op = rect
+    elif split[1] == 'row':
+        a = int(split[2].strip('y='))
+        b = int(split[4])
+        op = rot_row
+    else:
+        a = int(split[2].strip('x='))
+        b = int(split[4])
+        op = rot_col
+
+    return op, a, b
+
+
+def rect(grid, a, b):
+    for y in range(b):
+        for x in range(a):
+            grid[y][x] = '#'
+    return grid
+
+
+def rot_row(grid, a, b):
+    grid[a] = grid[a][-b:] + grid[a][:-b]
+    return grid
+
+
+def rot_col(grid, a, b):
+    grid = rot_row(list(zip(*grid)), a, b)
+    return [list(i) for i in list(zip(*grid))]
+
+
+def str_grid(grid):
+    out = '+' + '-' * 50 + '+\n'
+    for row in grid:
+        out += '|' + ''.join(row) + '|\n'
+    out += '+' + '-' * 50 + '+\n'
+    return out
 
 
 def get_input(filename):
