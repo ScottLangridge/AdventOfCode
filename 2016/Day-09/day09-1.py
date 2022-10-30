8f155633f96b6d8ad4729af35703c246000782ce
scott <scott@derossi.consulting>
2022-10-30 11:50:07 +0000

    Solved day 9

--- 2016/Day-09/day09-1.py
+++ 2016/Day-09/day09-1.py
@@ -1,10 +1,38 @@
 def main(raw_input):
-    # Parse input
+    seq = list(raw_input.strip())
+    return len(decompress(seq))
 
-    # Solve problem
 
-    # Return solution
-    return None
+def decompress(seq):
+    out = ""
+    while seq:
+        c = seq.pop(0)
+
+        # If the start of a marker
+        if c == '(':
+            # Build whole marker
+            str_marker = ''
+            while True:
+                c = seq.pop(0)
+                if c == ')':
+                    break
+                else:
+                    str_marker += c
+
+            marker = [int(i) for i in str_marker.split('x')]
+
+            # Get marked text
+            str_copy = ''.join(seq[:marker[0]])
+            seq = seq[marker[0]:]
+
+            # Repeat marked text
+            for i in range(marker[1]):
+                out += str_copy
+
+        else:
+            out += c
+
+    return out
 
 
 def get_input(filename):
--- 2016/Day-09/day09-2.py
+++ 2016/Day-09/day09-2.py
@@ -1,10 +1,33 @@
 def main(raw_input):
-    # Parse input
+    seq = raw_input.strip()
+    return count_decompress_len(seq)
 
-    # Solve problem
+def count_decompress_len(seq):
+    count = 0
 
-    # Return solution
-    return None
+    while seq:
+        # Before first marker
+        while seq and seq[0] != '(':
+            count += 1
+            seq = seq[1:]
+
+        # Read marker and count sub_seq
+        if seq:
+            sub_seq, mult, marker_len = read_marker(seq)
+            count += count_decompress_len(sub_seq) * mult
+            seq = seq[len(sub_seq) + marker_len:]
+
+    return count
+
+
+
+
+# seq = (NxM)...
+def read_marker(seq):
+    chars, mult = seq.split(')')[0].strip('(').split('x')
+    r = ')'.join(seq.split(')')[1:])[:int(chars)]
+    marker_len = len(seq.split(')')[0]) + 1
+    return r, int(mult), marker_len
 
 
 def get_input(filename):
