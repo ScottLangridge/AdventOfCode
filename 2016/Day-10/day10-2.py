5dfea7396954496f5901430d43a14a32f75c1295
scott <scott@derossi.consulting>
2022-10-30 12:47:47 +0000

    Solved day 10

--- 2016/Day-10/day10-2.py
+++ 2016/Day-10/day10-2.py
@@ -1,10 +1,90 @@
+from collections import defaultdict
+
+
+class Output:
+    def __init__(self, name):
+        self.name = name
+        self.inventory = []
+
+    def to_str(self):
+        return f'{self.name}: {self.inventory}'
+
+    def ready(self):
+        return False
+
+    def compares(self):
+        return None
+
+
+class Bot:
+    def __init__(self):
+        self.name = ""
+        self.inventory = []
+        self.lo = None
+        self.hi = None
+        self.compares = None
+
+    def to_str(self):
+        return f'{self.name}: {self.lo} | {self.hi}, {self.inventory}'
+
+    def load_instruction(self, instruction):
+        self.name = bot_name(instruction)
+        if instruction.startswith("value "):
+            self.inventory.append(int(instruction.split()[1]))
+        else:
+            self.lo = ' '.join(instruction.split()[5:7])
+            self.hi = ' '.join(instruction.split()[10:])
+
+    def ready(self):
+        return len(self.inventory) == 2
+
+    def operate(self, bots):
+        self.inventory = sorted(self.inventory)
+        self.compares = self.inventory[:]
+        bots[self.lo].inventory.append(self.inventory.pop(0))
+        bots[self.hi].inventory.append(self.inventory.pop(0))
+        bots[self.name] = self
+
+
 def main(raw_input):
-    # Parse input
+    instructions = raw_input.strip().splitlines()
+    bots = initialise_bots(instructions)
+    bots = bots | initialise_outputs(bots)
 
-    # Solve problem
+    continue_loop = True
+    while continue_loop:
+        continue_loop = False
+        for bot in bots:
+            bot = bots[bot]
+            if bot.ready():
+                bot.operate(bots)
+                continue_loop = True
 
-    # Return solution
-    return None
+    return bots["output 0"].inventory[0] * bots["output 1"].inventory[0] * bots["output 2"].inventory[0]
+
+
+def initialise_bots(instructions):
+    bot_hash = defaultdict(Bot)
+    for i in instructions:
+        bot_hash[bot_name(i)].load_instruction(i)
+    return bot_hash
+
+
+def initialise_outputs(bots):
+    output_hash = {}
+    for bot in bots.values():
+        if bot.lo.startswith("output"):
+            output_hash[bot.lo] = Output(bot.lo)
+        if bot.hi.startswith("output"):
+            output_hash[bot.hi] = Output(bot.hi)
+    return output_hash
+
+
+def bot_name(instruction):
+    if instruction.startswith("value "):
+        return " ".join(instruction.split()[4:])
+    else:
+        return " ".join(instruction.split()[:2])
 
 
 def get_input(filename):
