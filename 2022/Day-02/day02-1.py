ab7efded93067a2df047993886dde4563fbb2825
scott <scott@derossi.consulting>
2022-12-02 09:22:22 +0000

    Solved day 2

--- 2022/Day-02/day02-1.py
+++ 2022/Day-02/day02-1.py
@@ -1,10 +1,29 @@
 def main(raw_input):
-    # Parse input
+    match = [i.split() for i in raw_input.strip().splitlines()]
 
-    # Solve problem
+    total_score = 0
+    for game in match:
+        total_score += score_game(*game)
 
-    # Return solution
-    return None
+    return total_score
+
+
+def score_game(opponent_move, player_move):
+    player = {'X': 'R', 'Y': 'P', 'Z': 'S'}
+    opponent = {'A': 'R', 'B': 'P', 'C': 'S'}
+    beats = {'S': 'R', 'P': 'S', 'R': 'P'}
+    score = {'R': 1, 'P': 2, 'S': 3}
+
+    p = player[player_move]
+    o = opponent[opponent_move]
+
+    score = score[p]
+    if beats[o] == p:
+        score += 6
+    elif p == o:
+        score += 3
+
+    return score
 
 
 def get_input(filename):
--- 2022/Day-02/day02-2.py
+++ 2022/Day-02/day02-2.py
@@ -1,10 +1,38 @@
+1533
+
+
 def main(raw_input):
-    # Parse input
+    match = [i.split() for i in raw_input.strip().splitlines()]
 
-    # Solve problem
+    total_score = 0
+    for game in match:
+        total_score += score_game(*game)
 
-    # Return solution
-    return None
+    return total_score
+
+
+def score_game(opponent_move, desired_outcome):
+    beats = {'S': 'R', 'P': 'S', 'R': 'P'}
+    beaten_by = {y: x for x, y in beats.items()}
+    score = {'R': 1, 'P': 2, 'S': 3}
+
+    opponent = {'A': 'R', 'B': 'P', 'C': 'S'}
+    o = opponent[opponent_move]
+
+    if desired_outcome == 'X':
+        p = beaten_by[o]
+    elif desired_outcome == 'Y':
+        p = o
+    else:
+        p = beats[o]
+
+    score = score[p]
+    if beats[o] == p:
+        score += 6
+    elif p == o:
+        score += 3
+
+    return score
 
 
 def get_input(filename):
