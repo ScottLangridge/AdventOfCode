939b4e7a5f817bd79ad4a841de1c1265a73bb8dd
scott <scott@derossi.consulting>
2022-12-07 14:34:01 +0000

    2022 days 7

--- 2022/Day-07/day07-1.py
+++ 2022/Day-07/day07-1.py
@@ -1,10 +1,46 @@
+class Dir:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.children = []
+
+    def get_size(self):
+        return sum(map(lambda i: i.get_size(), self.children))
+
+
+class File:
+    def __init__(self, str_file):
+        size, self.name = str_file.split()
+        self.size = int(size)
+
+    def get_size(self):
+        return self.size
+
+
 def main(raw_input):
-    # Parse input
+    terminal = raw_input.splitlines()[1:]
+    root = Dir("/", None)
 
-    # Solve problem
+    current_dir = root
+    all_dirs = [root]
+    for row in terminal:
+        if row == "$ cd ..":
+            current_dir = current_dir.parent
+        elif row.startswith("$ cd"):
+            new_dir = Dir(row.split()[-1], current_dir)
+            all_dirs.append(new_dir)
+            current_dir.children.append(new_dir)
+            current_dir = new_dir
+        elif row[0].isdigit():
+            current_dir.children.append(File(row))
 
-    # Return solution
-    return None
+    sum_size = 0
+    for i in all_dirs:
+        size = i.get_size()
+        if size <= 100000:
+            sum_size += size
+
+    return sum_size
 
 
 def get_input(filename):
--- 2022/Day-07/day07-2.py
+++ 2022/Day-07/day07-2.py
@@ -1,10 +1,47 @@
+class Dir:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.children = []
+        self.size = None
+
+    def get_size(self):
+        if self.size is None:
+            self.size = sum(map(lambda i: i.get_size(), self.children))
+        return self.size
+
+
+class File:
+    def __init__(self, str_file):
+        size, self.name = str_file.split()
+        self.size = int(size)
+
+    def get_size(self):
+        return self.size
+
+
 def main(raw_input):
-    # Parse input
+    terminal = raw_input.splitlines()[1:]
+    root = Dir("/", None)
 
-    # Solve problem
+    current_dir = root
+    all_dirs = [root]
+    for row in terminal:
+        if row == "$ cd ..":
+            current_dir = current_dir.parent
+        elif row.startswith("$ cd"):
+            new_dir = Dir(row.split()[-1], current_dir)
+            all_dirs.append(new_dir)
+            current_dir.children.append(new_dir)
+            current_dir = new_dir
+        elif row[0].isdigit():
+            current_dir.children.append(File(row))
 
-    # Return solution
-    return None
+    unused = 70000000 - root.get_size()
+    must_clear = 30000000 - unused
+    for i in sorted(all_dirs, key=lambda d: d.get_size()):
+        if i.get_size() >= must_clear:
+            return i.get_size()
 
 
 def get_input(filename):
