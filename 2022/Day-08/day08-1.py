9058242b4ebb2090b5ee552bb82d25899ffaf4af
scott <scott@derossi.consulting>
2022-12-08 14:23:45 +0000

    2022 day 8... Stupidly

--- 2022/Day-08/day08-1.py
+++ 2022/Day-08/day08-1.py
@@ -1,10 +1,48 @@
 def main(raw_input):
-    # Parse input
+    forest = [list(map(int, line)) for line in raw_input.splitlines()]
+    max_y = len(forest)
+    max_x = len(forest[0])
 
-    # Solve problem
+    visible_count = 0
+    for y in range(max_y):
+        for x in range(max_x):
+            if tree_visible(forest, x, y, max_x, max_y):
+                visible_count += 1
 
-    # Return solution
-    return None
+    return visible_count
+
+
+def tree_visible(forest, tree_x, tree_y, max_x, max_y):
+    if tree_x == 0 or tree_x == max_x - 1 or tree_y == 0 or tree_y == max_y - 1:
+        return True
+
+    height = forest[tree_y][tree_x]
+
+    obscuring_trees = []
+    for x in range(tree_x):
+        obscuring_trees.append(forest[tree_y][x])
+    if max(obscuring_trees) < height:
+        return True
+
+    obscuring_trees = []
+    for x in range(tree_x + 1, max_x):
+        obscuring_trees.append(forest[tree_y][x])
+    if max(obscuring_trees) < height:
+        return True
+
+    obscuring_trees = []
+    for y in range(tree_y):
+        obscuring_trees.append(forest[y][tree_x])
+    if max(obscuring_trees) < height:
+        return True
+
+    obscuring_trees = []
+    for y in range(tree_y + 1, max_y):
+        obscuring_trees.append(forest[y][tree_x])
+    if max(obscuring_trees) < height:
+        return True
+
+    return False
 
 
 def get_input(filename):
--- 2022/Day-08/day08-2.py
+++ 2022/Day-08/day08-2.py
@@ -1,10 +1,63 @@
 def main(raw_input):
-    # Parse input
+    forest = [list(map(int, line)) for line in raw_input.splitlines()]
+    max_y = len(forest)
+    max_x = len(forest[0])
 
-    # Solve problem
+    visible_count = 0
+    best_score = 0
+    for y in range(max_y):
+        for x in range(max_x):
+            score = scenic_score(forest, x, y, max_x, max_y)
+            if score > best_score:
+                best_score = score
 
-    # Return solution
-    return None
+    return best_score
+
+
+def scenic_score(forest, tree_x, tree_y, max_x, max_y):
+    if tree_x == 0 or tree_x == max_x - 1 or tree_y == 0 or tree_y == max_y - 1:
+        return 0
+
+    height = forest[tree_y][tree_x]
+    views = []
+
+    view_score = 0
+    x = tree_x + 1
+    while x < max_x:
+        view_score += 1
+        if forest[tree_y][x] >= height:
+            break
+        x += 1
+    views.append(view_score)
+
+    view_score = 0
+    x = tree_x - 1
+    while x >= 0:
+        view_score += 1
+        if forest[tree_y][x] >= height:
+            break
+        x -= 1
+    views.append(view_score)
+
+    view_score = 0
+    y = tree_y + 1
+    while y < max_y:
+        view_score += 1
+        if forest[y][tree_x] >= height:
+            break
+        y += 1
+    views.append(view_score)
+
+    view_score = 0
+    y = tree_y - 1
+    while y >= 0:
+        view_score += 1
+        if forest[y][tree_x] >= height:
+            break
+        y -= 1
+    views.append(view_score)
+
+    return views[0] * views[1] * views[2] * views[3]
 
 
 def get_input(filename):
