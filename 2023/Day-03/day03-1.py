3b35b5186f83030031fd3e6a2123c4bc15c6ff5d
scott <scott@derossi.consulting>
2023-12-03 17:26:18 +0000

    Solved day 3

--- 2023/Day-03/day03-1.py
+++ 2023/Day-03/day03-1.py
@@ -1,16 +1,52 @@
 def main(raw_input):
-    # Parse input
+    schematic = [list(i) for i in raw_input.splitlines()]
 
-    # Solve problem
+    in_number = False
+    numbers = []
+    for y, row in enumerate(schematic):
+        for x, char in enumerate(row):
+            if char.isnumeric() and in_number is False:
+                numbers.append(detect_number(schematic, x, y))
+                in_number = True
+            elif not char.isnumeric():
+                in_number = False
 
-    # Return solution
-    return None
+    part_numbers = []
+    for number in numbers:
+        for x, y in number[1]:
+            if neighbor_contains_symbol(schematic, x, y):
+                part_numbers.append(number[0])
+                break
+
+    return sum(part_numbers)
 
 
 def get_input(filename):
     with open(filename) as f:
         raw_input = f.read()
     return raw_input
+
+
+def detect_number(schematic, first_x, y):
+    str_num = ''
+
+    x = first_x
+    coords = []
+    while x < len(schematic[0]) and schematic[y][x].isnumeric():
+        str_num += schematic[y][x]
+        coords.append((x, y))
+        x += 1
+
+    return int(str_num), coords
+
+
+def neighbor_contains_symbol(schematic, x, y):
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            if 0 <= y + dy < len(schematic) and 0 <= x + dx < len(schematic[0]):
+                if schematic[y + dy][x + dx] not in '.1234567890':
+                    return True
+    return False
 
 
 if __name__ == '__main__':
--- 2023/Day-03/day03-2.py
+++ 2023/Day-03/day03-2.py
@@ -1,16 +1,48 @@
 def main(raw_input):
-    # Parse input
+    schematic = [list(i) for i in raw_input.splitlines()]
 
-    # Solve problem
+    gear_ratios = []
+    for y, row in enumerate(schematic):
+        for x, char in enumerate(row):
+            if schematic[y][x] == '*':
+                adjacent_part_numbers = list(get_adjacent_part_numbers(schematic, x, y))
+                if len(adjacent_part_numbers) == 2:
+                    gear_ratios.append(adjacent_part_numbers[0] * adjacent_part_numbers[1])
 
-    # Return solution
-    return None
+    return sum(gear_ratios)
 
 
 def get_input(filename):
     with open(filename) as f:
         raw_input = f.read()
     return raw_input
+
+
+def get_adjacent_part_numbers(schematic, x, y):
+    adjacent_part_numbers = set()
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            xdx = x + dx
+            ydy = y + dy
+            if 0 <= ydy < len(schematic) and 0 <= xdx < len(schematic[0]):
+                if schematic[ydy][xdx].isnumeric():
+                    adjacent_part_numbers.add(detect_number(schematic, xdx, ydy))
+
+    return adjacent_part_numbers
+
+
+def detect_number(schematic, first_x, y):
+    str_num = ''
+
+    x = first_x
+    while x - 1 >= 0 and schematic[y][x - 1].isnumeric():
+        x -= 1
+
+    while x < len(schematic[0]) and schematic[y][x].isnumeric():
+        str_num += schematic[y][x]
+        x += 1
+
+    return int(str_num)
 
 
 if __name__ == '__main__':
