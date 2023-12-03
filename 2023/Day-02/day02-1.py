eb7e58290178d296ec69897b9d1bdea4bb6d3da1
scott <scott@derossi.consulting>
2023-12-03 09:10:18 +0000

    Solved day 2

--- 2023/Day-02/day02-1.py
+++ 2023/Day-02/day02-1.py
@@ -1,16 +1,27 @@
 def main(raw_input):
-    # Parse input
 
-    # Solve problem
-
-    # Return solution
-    return None
+    return sum([id_if_possible({'red': 12, 'green': 13, 'blue': 14}, i) for i in raw_input.splitlines()])
 
 
 def get_input(filename):
     with open(filename) as f:
         raw_input = f.read()
     return raw_input
+
+
+def id_if_possible(total_cubes, str_game):
+    game_split = str_game.split(': ')
+    game_id = int(game_split[0].strip('Game '))
+    reveals = game_split[1].split('; ')
+    for reveal in reveals:
+        for colour_count in reveal.split(', '):
+            colour_split = colour_count.split(' ')
+            count = int(colour_split[0])
+            colour = colour_split[1]
+            if count > total_cubes[colour]:
+                return 0
+    return game_id
+
 
 
 if __name__ == '__main__':
--- 2023/Day-02/day02-2.py
+++ 2023/Day-02/day02-2.py
@@ -1,16 +1,29 @@
 def main(raw_input):
-    # Parse input
 
-    # Solve problem
-
-    # Return solution
-    return None
+    return sum([min_set_power(i) for i in raw_input.splitlines()])
 
 
 def get_input(filename):
     with open(filename) as f:
         raw_input = f.read()
     return raw_input
+
+
+def min_set_power(str_game):
+    min_set = {'red': 0, 'green': 0, 'blue': 0}
+
+    game_split = str_game.split(': ')
+    game_id = int(game_split[0].strip('Game '))
+    reveals = game_split[1].split('; ')
+    for reveal in reveals:
+        for colour_count in reveal.split(', '):
+            colour_split = colour_count.split(' ')
+            count = int(colour_split[0])
+            colour = colour_split[1]
+            if min_set[colour] < count:
+                min_set[colour] = count
+
+    return min_set['red'] * min_set['green'] * min_set['blue']
 
 
 if __name__ == '__main__':
